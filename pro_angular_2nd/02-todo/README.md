f304043ec89fd191fd213c6bb663d7efe1ebcb57
wefine <wang.xiaoren@zte.com.cn>
2017-04-11 19:23:42 +0800

    Use angular4-quickstart to rewrite 02 todo example.
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

---
+++ pro_angular_2nd/02-todo/README.md
@@ -0,0 +1,3 @@
+# 02 - Your First Angular App
+
+Use angular4-quickstart to rewrite.
---
+++ pro_angular_2nd/02-todo/src/app/model.ts
@@ -0,0 +1,22 @@
+﻿export class Model {
+    user: string;
+    items: TodoItem[];
+
+    constructor() {
+        this.user = "Adam";
+        this.items = [new TodoItem("Buy Flowers", false),
+            new TodoItem("Get Shoes", false),
+            new TodoItem("Collect Tickets", false),
+            new TodoItem("Call Joe", false)]
+    }
+}
+
+export class TodoItem {
+    action: string;
+    done: boolean;
+
+    constructor(action: string, done: boolean) {
+        this.action = action;
+        this.done = done;
+    }
+}
