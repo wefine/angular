f0fce76a97b5ac44eb5445256bb47aa63c7b5483
wefine <wang.xiaoren@zte.com.cn>
2017-04-17 19:30:31 +0800

    Adding Product Pagination
    Signed-off-by: wefine <wang.xiaoren@zte.com.cn>

--- pro_angular_2nd/ch07-SportsStore/src/app/store/store.component.ts
+++ pro_angular_2nd/ch07-SportsStore/src/app/store/store.component.ts
@@ -8,16 +8,19 @@ import { ProductRepositoryService } from "../product/product-repository.service"
     templateUrl: './store.component.html',
     styleUrls: ['./store.component.css']
 })
-export class StoreComponent implements OnInit {
+export class StoreComponent {
     public selectedCategory = null;
+    public productsPerPage = 4;
+    public selectedPage = 1;
 
     constructor(private repository: ProductRepositoryService) {
     }
 
     get products(): Product[] {
-        console.log(this.selectedCategory);
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
 
-        return this.repository.getProducts(this.selectedCategory);
+        return this.repository.getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories(): string[] {
@@ -28,7 +31,18 @@ export class StoreComponent implements OnInit {
         this.selectedCategory = newCategory;
     }
 
-    ngOnInit() {
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
     }
 
+    changePageSize(newSize: number) {
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
+    }
+
+    get pageNumbers(): number[] {
+        return Array(
+            Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)
+        ).fill(0).map((x, i) => i + 1);
+    }
 }
