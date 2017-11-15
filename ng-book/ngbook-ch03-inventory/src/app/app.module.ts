b859f0f1abceb792eb0c854a54eb71d2749ee0ce
wefine <wefine@aliyun.com>
2017-11-15 15:36:20 +0800

    ngbook-ch03-inventory

---
+++ ng-book/ngbook-ch03-inventory/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductImageComponent,
+    ProductDepartmentComponent,
+    PriceDisplayComponent,
+    ProductRowComponent,
+    ProductsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
